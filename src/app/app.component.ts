799896a063f92b3fa06f2127e623f8dbf343e4c8
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-04-07 20:55:43 -0300

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './components/header/header.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'todo-list';
-}
+export class AppComponent {}
